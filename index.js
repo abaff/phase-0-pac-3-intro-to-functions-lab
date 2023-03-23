fca7a4ae03b26c4a933d419fbd23507495ce3ce1
Allyson Baffour Owusu <boallyson@gmail.com>
2023-03-23 15:00:47 -0400

    Completed lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string){
+    return string.toLowerCase ()
+}
+
+function logShout(string){
+ //   return string.toUpperCase()
+    console.log(string.toUpperCase())
+}
+function logWhisper(string){
+// return string.toLowercase()
+console.log(string.toLowerCase())
+}  
+
+function sayHiToHeadphonedRoommate(string) {
+
+    switch(string) {
+    case string.toLowerCase():
+    return "I can't hear you!";
+    //break;
+    case string.toUpperCase():
+    return "YES INDEED!";
+    //break;
+    case string = "Let's have dinner together!":
+    return "I would love to!";
+    }
+}
+// switch statements
